5f846c48422a985d957932ecc45f5b3ab02d4d43
Kotaro <ino.kota0602@gmail.com>
2025-10-01 13:56:49 +0900

    chore: Prettier導入

--- lesson2.ts
+++ lesson2.ts
@@ -2,22 +2,28 @@ let hasVehicle = true;
 let count: number = 10;
 let negative: number = -0.5;
 let float: number = 3.14;
-let hello: string = "hello";
+let hello: string = 'hello';
 
 const person: {
-    name: string;
-     age: number;
-    } = {
-    name: "Jack",
-    age:27
-}
+  name: string;
+  age: number;
+} = {
+  name: 'Jack',
+  age: 27,
+};
 
 console.log(person.name);
 
 //１８-配列に型をつける
-const fruits = ["Apple", "Banana", "Grape"];
+const fruits = ['Apple', 'Banana', 'Grape'];
 console.log(fruits);
 
 //１９-Tuple型で配列作成
-const book: [string, number, boolean] = ["business", 1500, false];
+const book: [string, number, boolean] = ['business', 1500, false];
 console.log(book);
+
+//２０-Enum型で特定まとまったグループ限定で受け入れる列挙型
+const coffee = {
+  hot: true,
+  size,
+};
