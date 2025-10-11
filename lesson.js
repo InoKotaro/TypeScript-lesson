c448fa2a3cea27108682156ae4ee4f24c8b68402
Kotaro <ino.kota0602@gmail.com>
2025-10-11 17:38:08 +0900

    セクション3 / 40～42終了

--- lesson.js
+++
@@ -1,7 +0,0 @@
-// ctrlt+クリックで選択可
-import axios from "axios";
-let hello = "hello";
-console.log(hello);
-function add(a, b) {
-    return a + b;
-}
--- lesson3.ts
+++ lesson3.ts
@@ -23,3 +23,23 @@
 
 // ３９- tsconfig.jsonでlibを指定して、TypeScriptが用意している型の定義を追加する
 // デフォルトでは lib がコメントアウトされてる => target で指定されてる分で自動設定される
+
+// ４０- allowJs、checkJs、jsx、declaration、declarationMapの設定
+// declaration、declarationMapはtsファイルを共有する際にtrue化
+
+// ４１- SourceMapを使用して、ブラウザでTypeScriptを操作する方法
+// ブラウザ内デベロッパーツール等でtsファイルを操作する際に、ソースマップを使用してtsファイルを操作する
+// 要はjs => ts へ逆算可能になる
+
+// ４２-outDirとrootDir、removeComments、noEmit、downlevelIteration設定方法
+// "outDir": "./dist" と書けば tsc 実行で ./dist フォルダと一緒にjsファイルが作成される
+// ※無駄フォルダができないように作成される
+
+// "rootDir": "./src" と書けば 厳密に./src フォルダと一緒にtsファイルが作成される
+// ※ただし全ファイルを srcフォルダに格納する必要がある、しないとエラーになる
+
+// "removeComments": true と書けば コンパイル後jsファイル内にコメントを残さない
+
+// "noEmit": true と書けば コンパイル後jsファイルを作成しない、型チェックだけする
+
+// "downlevelIteration": true と書けば ES3,ES5限定で for of（繰り返し処理） を使用できる
