09ade84e29929504673e24f8ecae4056467072ba
Kotaro <ino.kota0602@gmail.com>
2025-10-24 09:26:43 +0900

    セクション4 / 50終了

--- lesson4.ts
+++ lesson4.ts
@@ -15,3 +15,44 @@ console.log(bob48);
 // ４９- JSにコンパイルされたclass
 // ES5 => コンストラクタ関数, ES6 => クラス構文（クラス使用でname: string;）が削除されてコンパイルされる
 
+// ==============未プッシュ、５１書き足し
+// ５０- classを定義してオブジェクトを作成する方法
+// クラスとは、オブジェクトの設計図でUserクラスを作ると、その型のオブジェクトを量産できる
+
+class User50 {
+  // プロパティ（メンバー変数）
+  name: string;
+  age: number;
+
+  // コンストラクタ（初期化メソッド）
+  constructor(name: string, age: number) {
+    this.name = name; // this は作成中のオブジェクトを指す
+    this.age = age;
+  }
+
+  // メソッド（関数）
+  greet() {
+    console.log(`こんにちは、${this.name}です。${this.age}歳です。`);
+  }
+}
+
+const user1 = new User50('ユーザー１', 100);
+user1.greet();
+
+// こんにちは、ユーザーです。200歳です。 と出力
+
+// 設計図（クラス）から、いくつでもオブジェクト作成可能
+const user2 = new User50('ユーザー２', 200);
+const user3 = new User50('ユーザー３', 300);
+
+user2.greet();
+user3.greet();
+// こんにちは、ユーザー２です。200歳です。
+// こんにちは、ユーザー３です。300歳です。 と出力
+
+// TypeScriptでは、クラス自体を型としても使用可能
+
+let user: User50;  // User型の変数として定義
+user = new User50("ユーザー４", 400);  // Userクラスのインスタンスを代入
+
+// ※new User50("ユーザー５", 500).greet(); 変数に代入しなくてもクラスは使える
