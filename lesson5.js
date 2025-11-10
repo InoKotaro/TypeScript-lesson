9d49f724377b9f5f71e7574e27cf4bb37eb54b45
Kotaro <ino.kota0602@gmail.com>
2025-11-10 13:44:30 +0900

    セクション5 / 68,69終了

--- lesson5.js
+++ lesson5.js
@@ -22,5 +22,57 @@ class Developer67 {
         console.log(message);
     }
 }
-const dev = new Developer67('Katherine', 34, 9);
-dev.greeting("test");
+const dev67 = new Developer67('Katherine', 34, 9);
+dev67.greeting('67-test');
+class Developer68 {
+    constructor(name, age, experience) {
+        this.name = name;
+        this.age = age;
+        this.experience = experience;
+    }
+    greeting(message) {
+        console.log(message);
+        console.log(this.name, this.age, this.experience);
+    }
+}
+// クラスを使わず、直接オブジェクトでHuman68の構造作成
+const tmpDeveloper68 = {
+    name: 'Jack',
+    age: 38,
+    experience: 5,
+    greeting(message) {
+        console.log(message);
+        console.log(this.name, this.age, this.experience);
+    },
+};
+// Human68型の変数にDeveloper68インスタンスを代入
+const user68 = new Developer68('Bob', 50, 30);
+// Human68型の変数にtmpDeveloper（構造が一致するためエラーなし）を代入
+const user68_2 = tmpDeveloper68;
+user68.greeting('68-test');
+user68_2.greeting('68_2-test');
+// implementsで継承してるDeveloper69にあるオブジェクトにはHuman69で設定してるreadonly影響しない
+class Developer69 {
+    constructor(name, age, experience) {
+        this.name = name;
+        this.age = age;
+        this.experience = experience;
+    }
+    greeting(message) {
+        console.log(message);
+        console.log(this.name, this.age, this.experience);
+    }
+}
+const tmpDeveloper69 = {
+    name: 'Jack',
+    age: 38,
+    experience: 5,
+    greeting(message) {
+        console.log(message);
+        console.log(this.name, this.age, this.experience);
+    },
+};
+// readonlyでも代入を経由すれば書換え可能
+let user69 = new Developer69('Bob', 50, 30);
+user69 = tmpDeveloper69;
+user69.greeting('69-readonly-test');
--- lesson5.ts
+++ lesson5.ts
@@ -51,5 +51,79 @@ class Developer67 implements Human67 {
   }
 }
 
-const dev = new Developer67('Katherine', 34, 9);
-dev.greeting("test");
+const dev67 = new Developer67('Katherine', 34, 9);
+dev67.greeting('67-test');
+
+// ６８- TypeScriptの構造的部分型
+interface Human68 {
+  name: string;
+  age: number;
+  greeting(message: string): void;
+}
+
+class Developer68 implements Human68 {
+  constructor(
+    public name: string,
+    public age: number,
+    public experience: number,
+  ) {}
+
+  greeting(message: string) {
+    console.log(message);
+    console.log(this.name, this.age, this.experience);
+  }
+}
+
+// クラスを使わず、直接オブジェクトでHuman68の構造作成
+const tmpDeveloper68 = {
+  name: 'Jack',
+  age: 38,
+  experience: 5,
+  greeting(message: string) {
+    console.log(message);
+    console.log(this.name, this.age, this.experience);
+  },
+};
+
+// Human68型の変数にDeveloper68インスタンスを代入
+const user68: Human68 = new Developer68('Bob', 50, 30);
+// Human68型の変数にtmpDeveloper（構造が一致するためエラーなし）を代入
+const user68_2: Human68 = tmpDeveloper68;
+user68.greeting('68-test');
+user68_2.greeting('68_2-test');
+
+// ６９- readonly修飾子をinterfaceに使って読むだけのプロパティを作成
+interface Human69 {
+  readonly name: string;
+  age: number;
+  greeting(message: string): void;
+}
+
+// implementsで継承してるDeveloper69にあるオブジェクトにはHuman69で設定してるreadonly影響しない
+class Developer69 implements Human69 {
+  constructor(
+    public name: string,
+    public age: number,
+    public experience: number,
+  ) {}
+
+  greeting(message: string) {
+    console.log(message);
+    console.log(this.name, this.age, this.experience);
+  }
+}
+
+const tmpDeveloper69 = {
+  name: 'Jack',
+  age: 38,
+  experience: 5,
+  greeting(message: string) {
+    console.log(message);
+    console.log(this.name, this.age, this.experience);
+  },
+};
+
+// readonlyでも代入を経由すれば書換え可能
+let user69: Human69 = new Developer69('Bob', 50, 30);
+user69 = tmpDeveloper69;
+user69.greeting('69-readonly-test');
