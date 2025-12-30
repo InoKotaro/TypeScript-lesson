accdc67d6ff9f1c76b2846dd56871542702b0d5a
Kotaro <ino.kota0602@gmail.com>
2025-12-30 14:26:20 +0900

    セクション7 / 99終了

--- lesson7.ts
+++ lesson7.ts
@@ -1,8 +1,9 @@
 // ９８- ジェネリクスを使って、型を引数として受け取る
 // valueは変数名
 // 例では<T>で型推論される
-function copy<T>(value: T): T {
+// ９９- extendsを使って型パラメータに制約可能
+function copy<T extends { name: string }>(value: T): T {
   return value;
 }
 
-console.log(copy<string>("hello"));
+console.log(copy({ name: 'Jack' }));
